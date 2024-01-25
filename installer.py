ab5101e4f93f278e870032a01547042c01ec1d00
turbotriggerbot <turbotriggerbot@proton.me>
2024-01-25 23:43:31 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,4 +1,18 @@
 import os
+import customtkinter
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def create_python_file(directory, filename):
     content = '''
@@ -51,12 +65,12 @@ exec(open(file_path).read())
 
 def create_batch_file(directory, python_file):
     batch_content = f'cd "{directory}"\npython "{python_file}"'
-    batch_file_path = os.path.join(os.path.expanduser("~"), "Desktop", "run_script.bat")
+    batch_file_path = os.path.join(os.path.expanduser("~"), "Desktop", "runner.bat")
     with open(batch_file_path, 'w') as batch_file:
         batch_file.write(batch_content)
     return batch_file_path
 
-def main():
+def install():
     # Directory to save the Python file
     ## save_directory = input("Enter the directory to save the Python file:\n")
     ## make it desktop by default instead 
@@ -75,5 +89,64 @@ def main():
     batch_file_path = create_batch_file(save_directory, python_file_name)
     print(f"Batch file saved on desktop: {batch_file_path}")
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+
+import time
+app = customtkinter.CTk()
+app.geometry("370x300")
+app.title("TurboTrigger installer")
+app.attributes('-topmost', True)
+
+text = customtkinter.CTkLabel(app, text="Install TurboTrigger?", fg_color="transparent" , font=('consola', 14, 'bold'))
+text.grid(row=0 , column=0  , columnspan=2 , padx=20 , pady=20)
+
+
+
+desc = customtkinter.CTkLabel(app, text="the following files will be added to your desktop : \n\n   turbo.py\n   runner.bat",justify="left" , fg_color="transparent" , font=('consola', 14))
+desc.grid(row=1 , column=0  , columnspan=2 , padx=20 , pady=20)
+
+
+def button_event():
+    print("button pressed")
+    install()
+    time.sleep(1)
+    button.after(100 , lambda: button.destroy())
+    button2.after(100 , lambda: button2.destroy())
+    desc.after(100 , lambda: desc.destroy())
+    text.configure(text="Installed successfully" )
+
+    button3.grid(row=2 , column=0 , columnspan=2 , padx=80 , pady=(100 , 0))
+    
+
+
+
+button = customtkinter.CTkButton(app, text="yes", command=button_event)
+button.grid(row=2 , column=0 , padx=20 , pady=20)
+
+def button_event2():
+    
+    print("button pressed")
+    exit()
+
+
+button2 = customtkinter.CTkButton(app, text="no", command=button_event2)
+button2.grid(row=2 , column=1 , padx=20 , pady=20)
+
+
+def button_event3():
+    
+    print("button pressed")
+    exit()
+
+
+button3 = customtkinter.CTkButton(app, width=200 ,text="finish", command=app.destroy)
+
+
+app.mainloop()
+
+
+
